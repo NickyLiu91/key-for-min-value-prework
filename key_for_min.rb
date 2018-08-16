af12dbe7b44abbff61f1072affe4750c0ecd48e9
Nicky Liu <nickyliu91@gmail.com>
2018-08-16 09:22:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-    smallest_value = 0
+    smallest_value = nil
     current_key = ""
     name_hash.each do |key, value|
-      if smallest_value < value
+      if smallest_value < value || smallest_value == nil
         smallest_value = value
         current_key = key
       end
